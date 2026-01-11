efdc4c23f4b6664ca35410dfdfcde866fbe8d212
Ali Elbanna <alielbana6662@gmail.com>
2026-01-11 18:36:37 +0200

    -add date formatter

---
+++ lib/core/utils/date_formatter.dart
@@ -0,0 +1,41 @@
+import 'package:intl/intl.dart';
+
+class DateFormatter {
+  static String formatMessageTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
+
+    if (difference.inDays == 0) {
+      return DateFormat('HH:mm').format(dateTime);
+    } else if (difference.inDays == 1) {
+      return 'Yesterday';
+    } else if (difference.inDays < 7) {
+      return DateFormat('EEEE').format(dateTime);
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
+
+  static String formatChatListTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
+
+    if (difference.inMinutes < 1) {
+      return 'Just now';
+    } else if (difference.inHours < 1) {
+      return '${difference.inMinutes}m ago';
+    } else if (difference.inDays == 0) {
+      return DateFormat('HH:mm').format(dateTime);
+    } else if (difference.inDays == 1) {
+      return 'Yesterday';
+    } else if (difference.inDays < 7) {
+      return DateFormat('EEE').format(dateTime);
+    } else {
+      return DateFormat('dd/MM/yy').format(dateTime);
+    }
+  }
+
+  static String formatDetailedTime(DateTime dateTime) {
+    return DateFormat('dd MMM yyyy, HH:mm').format(dateTime);
+  }
+}
